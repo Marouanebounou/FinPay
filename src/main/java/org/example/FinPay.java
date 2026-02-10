599ed11f3473b12f1df2d206aad1a2678be2cf8c
marouane bounou <maroinbounou.123@gmail.com>
2026-02-10 20:58:22 +0100

    Create finpay and Statistique classes and constructors

--- src/main/java/org/example/FinPay.java
+++ src/main/java/org/example/FinPay.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FinPay {
@@ -8,5 +9,63 @@ public class FinPay {
     private static int idCounter = 1;
     private List<Facture> factures;
     private List<Client> clients;
+    private List<Prestataire> prestataires;
+    private Statistique statistique;
 
+    public FinPay(String domaine) {
+        this.id = idCounter++;
+        this.statistique = new Statistique(this);
+        this.domaine = domaine;
+        this.factures = new ArrayList<>();
+        this.clients = new ArrayList<>();
+        this.prestataires = new ArrayList<>();
+    }
+
+    public Statistique getStatistique() {
+        return statistique;
+    }
+
+    public void setStatistique(Statistique statistique) {
+        this.statistique = statistique;
+    }
+
+    public String getDomaine() {
+        return domaine;
+    }
+
+    public void setDomaine(String domaine) {
+        this.domaine = domaine;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Facture> getFactures() {
+        return factures;
+    }
+
+    public void setFactures(List<Facture> factures) {
+        this.factures = factures;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public List<Prestataire> getPrestataires() {
+        return prestataires;
+    }
+
+    public void setPrestataires(List<Prestataire> prestataires) {
+        this.prestataires = prestataires;
+    }
 }
---
+++ src/main/java/org/example/Statistique.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Statistique {
+    private int id;
+    private FinPay finPay;
+    private static int idCounter = 1;
+    private List<Facture> factureList;
+    private BigDecimal totalAmount;
+    private BigDecimal totalComission;
+    private List<Client> clients;
+    private List<Prestataire> prestataires;
+
+    public Statistique(FinPay finPay) {
+        this.finPay = finPay;
+        this.id = idCounter++;
+        this.factureList = new ArrayList<>();
+        this.totalAmount = BigDecimal.valueOf(0);
+        this.totalComission = BigDecimal.valueOf(0);
+        this.clients = new ArrayList<>();
+        this.prestataires = new ArrayList<>();
+
+    }
+
+    public FinPay getFinPay() {
+        return finPay;
+    }
+
+    public void setFinPay(FinPay finPay) {
+        this.finPay = finPay;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Facture> getFactureList() {
+        return factureList;
+    }
+
+    public void setFactureList(List<Facture> factureList) {
+        this.factureList = factureList;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public BigDecimal getTotalComission() {
+        return totalComission;
+    }
+
+    public void setTotalComission(BigDecimal totalComission) {
+        this.totalComission = totalComission;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public List<Prestataire> getPrestataires() {
+        return prestataires;
+    }
+
+    public void setPrestataires(List<Prestataire> prestataires) {
+        this.prestataires = prestataires;
+    }
+}
