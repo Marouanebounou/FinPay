e5dcb156c2655b21359729392df7f941c9da3d5b
Rida1019-taki <takirida72@gmail.com>
2026-02-16 10:39:23 +0100

    version1

--- src/main/java/org/example/util/PdfGenerator.java
+++ src/main/java/org/example/util/PdfGenerator.java
@@ -1,4 +1,18 @@
 package org.example.util;
 
+import org.example.model.Client;
+import org.example.model.Facture;
+
 public class PdfGenerator {
+
+    public static void generateFacturePDF(Facture facture, Client client) {
+        System.out.println("=== PDF Fichier ===");
+        System.out.println("Client : " + client.getName() + " | Email: " + client.getEmail());
+        System.out.println("Facture ID : " + facture.getId());
+        System.out.println("Montant : " + facture.getBalance() + " MAD");
+        System.out.println("Statut : " + facture.getStatus());
+        System.out.println("===================");
+        System.out.println("PDF généré avec succès !");
+    }
 }
+
