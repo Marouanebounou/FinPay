438744412a4d4a64fb576a00d99945b5641d72c9
Rida1019-taki <takirida72@gmail.com>
2026-02-27 11:29:40 +0000

    test 2

--- src/test/java/org/example/Calcultotalfacturesprestataire.java
+++ src/test/java/org/example/Calcultotalfacturesprestataire.java
@@ -1,31 +1,42 @@
 package org.example;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import java.util.Arrays;
+import org.example.model.Facture;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.example.config.DatabaseConfig.getConnection;
 
 public class Calcultotalfacturesprestataire {
 
-    public double calculerTotal(List<Double> montants) {
-        if (montants == null || montants.isEmpty()) {
-            return 0.0;
-        }
-        double total = 0.0;
-        for (Double m : montants) {
-            if (m != null) total += m;
-        }
-        return total;
-    }
+    public static List<Facture> totalFacturesChaquePrestataire() {
+        List<Facture> facturesResultat = new ArrayList<>();
 
-    @Test
-    @DisplayName("Tester la fonction callerTotal")
-    void testCalculerTotal(){
-        Calcultotalfacturesprestataire calcu = new Calcultotalfacturesprestataire();
-        List<Double> list = Arrays.asList(1000.0);
-        double total = calcu.calculerTotal(list);
-        assertEquals(1000.0 , total , "not working");
+        // Hna beddelna idInvoice b id hit hia li 3ndek f SQL
+        String query = "SELECT f.id_pre, COUNT(f.id) as nombreFacture, " +
+                "ROUND(SUM(f.balance),2) as sommeMontant " +
+                "FROM facture f " +
+                "GROUP BY f.id_pre " +
+                "ORDER BY f.id_pre";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                int idPre = rs.getInt("id_pre");
+                int nb = rs.getInt("nombreFacture");
+                BigDecimal total = rs.getBigDecimal("sommeMontant");
+                facturesResultat.add(new Facture(idPre, nb, total));
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL : " + e.getMessage());
+            e.printStackTrace();
+        }
+        return facturesResultat;
     }
 }
